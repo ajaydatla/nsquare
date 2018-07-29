40c1be27efea4e2d9d1fcf014057705388966c0c
ajay datla <ajaydatlalbn06@gmail.com>
2018-07-29 17:09:35 +0530

    started spring

--- src/main/java/com/nsquare/NsquareApplication.java
+++ src/main/java/com/nsquare/NsquareApplication.java
@@ -2,9 +2,10 @@ package com.nsquare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NsquareApplication {
+public class NsquareApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NsquareApplication.class, args);
